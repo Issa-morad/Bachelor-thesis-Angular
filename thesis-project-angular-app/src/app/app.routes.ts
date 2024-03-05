c1ef7ae4fff73963cdb3396ed9279a53f0bf4212
Issa-morad <issamorad94@gmail.com>
2024-03-05 14:53:11 +0100

    VideoPage

--- thesis-project-angular-app/src/app/app.routes.ts
+++ thesis-project-angular-app/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { StartComponent } from './start/start.component';
+import { VideoComponent } from './video/video.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '', pathMatch: 'full'},
-    { path: '', component: StartComponent }
+    { path: '', component: StartComponent },
+    {path:'video', component: VideoComponent}
 ];
