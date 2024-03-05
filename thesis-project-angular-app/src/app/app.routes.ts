08445700a2c308050d3854f5111afbcc0a299fd1
Issa-morad <issamorad94@gmail.com>
2024-03-05 13:46:52 +0100

    Stratrpage

--- thesis-project-angular-app/src/app/app.routes.ts
+++ thesis-project-angular-app/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { StartComponent } from './start/start.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '', pathMatch: 'full'},
+    { path: '', component: StartComponent }
+];
---
+++ thesis-project-angular-app/src/app/start.ts
@@ -0,0 +1,6 @@
+/*{"message":"https:\/\/images.dog.ceo\/breeds\/spaniel-sussex\/n02102480_582.jpg","status":"success"}*/
+
+export interface Start {
+    message: string;
+    status: string;
+}
