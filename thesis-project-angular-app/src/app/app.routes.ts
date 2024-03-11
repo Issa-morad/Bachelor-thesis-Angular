402e1819830cb3d4dda2b0453054354b45101d86
Jesper Li <Jesper.Li@jordbruksverket.se>
2024-03-11 12:29:22 +0100

    changed an error in the merge

--- thesis-project-angular-app/src/app/app.routes.ts
+++ thesis-project-angular-app/src/app/app.routes.ts
@@ -1,20 +1,16 @@
 import { Routes } from '@angular/router';
 
-import { StartComponent } from './start/start.component';
 import { AudioComponent } from './audio/audio.component';
 import { TextComponent } from './text/text.component';
-import { TempComp1Component } from './temp-comp-1/temp-comp-1.component';
-import { TempComp2Component } from './temp-comp-2/temp-comp-2.component';
 import { ImageLoaderComponent } from './image-loader/image-loader.component';
 import { VideoLoaderComponent } from './video-loader/video-loader.component';
+import { StartComponent } from './start/start.component';
 
 const amounts: number[] = [10,20,50,80]
 
 export const routes: Routes = [
     { path: '', component: StartComponent },
-    { path: 'temp1', component: TempComp1Component },
-    { path: 'temp2', component: TempComp2Component },
-    { path:'audio', component: AudioComponent },
+    { path: 'audio', component: AudioComponent },
     { path: 'text', component: TextComponent },
     { path: 'load-images/10', component: ImageLoaderComponent, data: { loadAmount: 10 }},
     { path: 'load-images/20', component: ImageLoaderComponent, data: { loadAmount: 20 }},
