05f0164bf6cf3c5f8470da7eedfb5658ea098ef1
Issa-morad <issamorad94@gmail.com>
2024-03-21 16:04:57 +0100

    testing stash

--- thesis-project-angular-app/src/app/app.config.ts
+++ thesis-project-angular-app/src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding()), provideClientHydration(), provideHttpClient()]
+  providers: [provideRouter(routes, withComponentInputBinding()), provideClientHydration(), provideHttpClient(), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
